48e4b1441662b6a79ea9fbec5e8b98b437bb45ab
Travis Clarke <travisjonathanclarke@gmail.com>
2019-06-29 16:21:25 -0400

    removed dist

--- ml-server/dist/controllers/users.controller.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUser = (req, res) => {
-    res.send('here is a user');
-};
-//# sourceMappingURL=users.controller.js.map
--- ml-server/dist/middleware/auth.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ensureAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated())
-        return next();
-    else
-        res.status(503).send('UNAUTHORIZED');
-};
-//# sourceMappingURL=auth.middleware.js.map
